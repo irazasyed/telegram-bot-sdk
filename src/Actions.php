b508e8fb877f6ee5e68ec387b29c2c21f7fd7a0f
Syed I.R <syed@lukonet.com>
2015-09-01 04:42:04 +0530

    Add Chat Actions Helper Class.

---
+++ src/Actions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Irazasyed\Telegram;
+
+class Actions
+{
+    /** @var string Set chat status as Typing */
+    const TYPING = 'typing';
+
+    /** @var string Set chat status as Sending Photo */
+    const UPLOAD_PHOTO = 'upload_photo';
+
+    /** @var string Set chat status as Sending Video */
+    const UPLOAD_VIDEO = 'upload_video';
+
+    /** @var string Set chat status as Sending Audio */
+    const UPLOAD_AUDIO = 'upload_audio';
+
+    /** @var string Set chat status as Sending Document */
+    const UPLOAD_DOCUMENT = 'upload_document';
+
+    /** @var string Set chat status as Choosing Geo */
+    const FIND_LOCATION = 'find_location';
+
+    /** @var string Set chat status as Recording Video */
+    const RECORD_VIDEO = 'record_video';
+}
