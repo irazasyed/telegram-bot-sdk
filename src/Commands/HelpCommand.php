91816151bb9534b915ee63e8d6dc3deeeaac2ca9
Syed I.R <syed@lukonet.com>
2015-09-01 05:24:39 +0530

    Add Default Help Command.

---
+++ src/Commands/HelpCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Irazasyed\Telegram\Commands;
+
+class HelpCommand extends Command
+{
+    /**
+     * @var string Command Name
+     */
+    protected $name = "help";
+
+    /**
+     * @var string Command Description
+     */
+    protected $description = "Help command, Get a list of commands";
+
+    /**
+     * @inheritdoc
+     */
+    public function handle($arguments)
+    {
+        $commands = $this->telegram->getCommands();
+
+        $response = '';
+        foreach ($commands as $name => $handler) {
+            $response .= sprintf('/%s - %s' . PHP_EOL, $name, $handler->getDescription());
+        }
+
+        $this->replyWithMessage($response);
+    }
+}
