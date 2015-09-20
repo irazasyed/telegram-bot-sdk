002633187bb9abd5a1b103b916a5e2bee2d2cdfa
Jonathan Williamson <jonnywilliamson@gmail.com>
2015-09-20 10:26:06 +0100

    Add new getFile method

---
+++ src/Objects/File.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+/**
+ * Class File
+ *
+ * @package Telegram\Bot\Objects
+ *
+ * @method string   getFileId()     Unique identifier for this file.
+ * @method int      getFileSize()   (Optional). File size, if known.
+ * @method string   getFilePath()   (Optional). File path. Use https://api.telegram.org/file/bot<token>/<file_path> to get the file.
+ */
+class File extends BaseObject
+{
+    /**
+     * @inheritdoc
+     */
+    public function relations()
+    {
+        return [];
+    }
+}
