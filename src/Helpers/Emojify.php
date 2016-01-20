047e1d40c7e3d31b2908bfc68352c3b2c1837493
Filipe Kiss <eu@filipekiss.com.br>
2016-01-20 18:31:29 -0200

    Fix whitespace and newline problems

--- src/Helpers/Emojify.php
+++ src/Helpers/Emojify.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Telegram\Bot\Helpers;
 
@@ -6,49 +6,53 @@ use Telegram\Bot\Exceptions\TelegramEmojiMapFileNotFoundException;
 
 class Emojify
 {
-  /**
-   * The path to the file containing the emoji map
-   * @var string
-   */
-  const EMOJI_MAP_FILE = '/../Storage/emoji.json';
-  
-  /**
-   * The object mapping the emoji to words
-   * @var object
-   */
-  protected $emojiMap;
-  
-  public function __construct($emojiMapFile = null) 
-  {
-    if (empty($emojiMapFile)) {
-      $emojiMapFile = __DIR__ . self::EMOJI_MAP_FILE;
-    }
-    if (!file_exists($emojiMapFile)) {
-      throw new TelegramEmojiMapFileNotFoundException();
-    }
-    $emojiMapFileContents = file_get_contents($emojiMapFile);
-    $this->emojiMap = (array) json_decode($emojiMapFileContents);
-    $this->wordMap = array_flip($this->emojiMap);
-  }
-  
-  public function toEmoji($text) {
-    return $this->replace($text, $this->emojiMap);
-  }
-  
-  public function toWord($text) {
-    return $this->replace($text, $this->wordMap, null);
-  }
-  
-  protected function replace($line, $replace, $startDelimiter = ':') {
-    uksort($replace, function($a, $b) {
-      return strlen($b) - strlen($a);
-    });
+    /**
+     * The path to the file containing the emoji map.
+     *
+     * @var string
+     */
+    const EMOJI_MAP_FILE = '/../Storage/emoji.json';
 
-    foreach ($replace as $key => $value) {
-        $line = str_replace($startDelimiter.$key, $value, $line);
+    /**
+    * The object mapping the emoji to words.
+    *
+    * @var object
+    */
+    protected $emojiMap;
+    
+    public function __construct($emojiMapFile = null)
+    {
+        if (empty($emojiMapFile)) {
+            $emojiMapFile = __DIR__.self::EMOJI_MAP_FILE;
+        }
+        if (!file_exists($emojiMapFile)) {
+            throw new TelegramEmojiMapFileNotFoundException();
+        }
+        $emojiMapFileContents = file_get_contents($emojiMapFile);
+        $this->emojiMap = (array) json_decode($emojiMapFileContents);
+        $this->wordMap = array_flip($this->emojiMap);
     }
 
-    return $line;
-  }
+    public function toEmoji($text)
+    {
+        return $this->replace($text, $this->emojiMap);
+    }
+
+    public function toWord($text)
+    {
+        return $this->replace($text, $this->wordMap, null);
+    }
+
+    protected function replace($line, $replace, $startDelimiter = ':')
+    {
+        uksort($replace, function ($a, $b) {
+            return strlen($b) - strlen($a);
+        });
+
+        foreach ($replace as $key => $value) {
+            $line = str_replace($startDelimiter.$key, $value, $line);
+        }
+
+        return $line;
+    }
 }
-
