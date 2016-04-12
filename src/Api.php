14d363bd21106f1382c4089eff7bfa907dba68a5
Jonathan Williamson <jonnywilliamson@gmail.com>
2016-04-12 23:14:37 +0100

    Add more message detection types.

--- src/Api.php
+++ src/Api.php
@@ -1006,6 +1006,7 @@ class Api
         }
 
         $types = [
+            'text',
             'audio',
             'document',
             'photo',
@@ -1015,10 +1016,17 @@ class Api
             'contact',
             'location',
             'venue',
-            'text',
             'new_chat_member',
             'left_chat_member',
+            'new_chat_title',
+            'new_chat_photo',
+            'delete_chat_photo',
+            'group_chat_created',
+            'supergroup_chat_created',
+            'channel_chat_created',
             'migrate_to_chat_id',
+            'migrate_from_chat_id',
+            'pinned_message',
         ];
 
         return $object->keys()
