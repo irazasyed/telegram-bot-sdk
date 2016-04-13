7786a25004550f5000c845aa4cc043c176222085
Ihor Polyakov <ihor.polyakov@gmail.com>
2016-04-13 23:55:31 +0300

    The groundwork for the future Telegram Bot API update
    Try to make post request in method was not found.

--- src/Api.php
+++ src/Api.php
@@ -10,6 +10,7 @@ use Telegram\Bot\HttpClients\GuzzleHttpClient;
 use Telegram\Bot\HttpClients\HttpClientInterface;
 use Telegram\Bot\Objects\File;
 use Telegram\Bot\Objects\Message;
+use Telegram\Bot\Objects\UnknownObject;
 use Telegram\Bot\Objects\Update;
 use Telegram\Bot\Objects\User;
 use Telegram\Bot\Objects\UserProfilePhotos;
@@ -1164,7 +1165,7 @@ class Api
      * @param $method
      * @param $arguments
      *
-     * @return bool|TelegramResponse
+     * @return bool|TelegramResponse|array
      */
     public function __call($method, $arguments)
     {
@@ -1185,8 +1186,9 @@ class Api
 
             return $response;
         }
+        $response = $this->post($method, $arguments[0]);
 
-        return false;
+        return new UnknownObject($response->getDecodedBody());
     }
 
     /**
