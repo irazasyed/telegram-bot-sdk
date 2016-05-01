4f81cdcc8a2ddeb7037f5f04482899975f622e4a
Sloothword <Sloothword@users.noreply.github.com>
2016-05-01 15:17:52 +0200

    Move type detection from Api class to Message and Update. (#177)
    * Move type detection from Api class to Message and Update.
    
    * Added nitpicks.
    
    * And another nitpick.

--- src/Objects/Message.php
+++ src/Objects/Message.php
@@ -83,5 +83,57 @@ class Message extends BaseObject
     {
         return $this->get('caption');
     }
+    
+    /**
+     * Determine if the message is of given type
+     *
+     * @param string         $type
+     *
+     * @return bool
+     */
+    public function isType($type)
+    {
+        if ($this->has(strtolower($type))) {
+            return true;
+        }
 
+        return $this->detectType() === $type;
+    }
+    
+    
+    /**
+     * Detect type based on properties.
+     *
+     * @return string|null
+     */
+    public function detectType()
+    {
+        $types = [
+            'text',
+            'audio',
+            'document',
+            'photo',
+            'sticker',
+            'video',
+            'voice',
+            'contact',
+            'location',
+            'venue',
+            'new_chat_member',
+            'left_chat_member',
+            'new_chat_title',
+            'new_chat_photo',
+            'delete_chat_photo',
+            'group_chat_created',
+            'supergroup_chat_created',
+            'channel_chat_created',
+            'migrate_to_chat_id',
+            'migrate_from_chat_id',
+            'pinned_message',
+        ];
+
+        return $this->keys()
+            ->intersect($types)
+            ->pop();
+    }
 }
