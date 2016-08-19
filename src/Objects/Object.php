1dc123f9836dbdf39871e45ba926167a1d396108
Jonathan Williamson <jonnywilliamson@gmail.com>
2016-08-19 13:23:14 +0100

    Fix Travis build issue.

--- src/Objects/Object.php
+++ src/Objects/Object.php
@@ -4,4 +4,13 @@ namespace Telegram\Bot\Objects;
 
 class Object extends BaseObject
 {
+    /**
+     * Property relations.
+     *
+     * @return array
+     */
+    public function relations()
+    {
+        return [];
+    }
 }
