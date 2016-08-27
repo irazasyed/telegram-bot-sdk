461ab21416cd3da1d638cdb4e234754e03387b81
Syed I.R <syed@lukonet.com>
2016-08-27 12:57:32 +0530

    Add getMessage to handle edited_message case.

--- src/Objects/Update.php
+++ src/Objects/Update.php
@@ -6,12 +6,16 @@ namespace Telegram\Bot\Objects;
  * Class Update.
  *
  *
- * @property int                  $updateId               The update's unique identifier. Update identifiers start from a certain positive number and increase sequentially.
- * @property Message              $message                (Optional). New incoming message of any kind - text, photo, sticker, etc.
- * @property EditedMessage        $editedMessage          (Optional). New version of a message that is known to the bot and was edited.
- * @property InlineQuery          $inlineQuery            (Optional). New incoming inline query.
- * @property ChosenInlineResult   $chosenInlineResult     (Optional). A result of an inline query that was chosen by the user and sent to their chat partner.
- * @property CallbackQuery        $callbackQuery          (Optional). Incoming callback query.
+ * @property int                $updateId               The update's unique identifier. Update identifiers start from a
+ *                                                      certain positive number and increase sequentially.
+ * @property Message            $message                (Optional). New incoming message of any kind - text, photo,
+ *                                                      sticker, etc.
+ * @property EditedMessage      $editedMessage          (Optional). New version of a message that is known to the bot
+ *                                                      and was edited.
+ * @property InlineQuery        $inlineQuery            (Optional). New incoming inline query.
+ * @property ChosenInlineResult $chosenInlineResult     (Optional). A result of an inline query that was chosen by the
+ *                                                      user and sent to their chat partner.
+ * @property CallbackQuery      $callbackQuery          (Optional). Incoming callback query.
  *
  * @link https://core.telegram.org/bots/api#update
  */
@@ -40,11 +44,11 @@ class Update extends BaseObject
     {
         return new static($this->last());
     }
-    
+
     /**
      * Determine if the update is of given type
      *
-     * @param string         $type
+     * @param string $type
      *
      * @return bool
      */
@@ -53,10 +57,10 @@ class Update extends BaseObject
         if ($this->has(strtolower($type))) {
             return true;
         }
-    
+
         return $this->detectType() === $type;
     }
-    
+
     /**
      * Detect type based on properties.
      *
@@ -75,5 +79,21 @@ class Update extends BaseObject
         return $this->keys()
             ->intersect($types)
             ->pop();
+    }
+
+    /**
+     * Returns message.
+     *
+     * @return null|EditedMessage|Message
+     */
+    public function getMessage()
+    {
+        if ($this->has('message')) {
+            return $this->message;
+        } elseif ($this->has('edited_message')) {
+            return $this->editedMessage;
+        }
+
+        return null;
     }
 }
