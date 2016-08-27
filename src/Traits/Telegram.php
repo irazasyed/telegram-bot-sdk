ab18886b5a56fbdf5dad78ea073c79fab6216849
Syed I.R <syed@lukonet.com>
2016-08-27 12:40:03 +0530

    Add telegram trait.

---
+++ src/Traits/Telegram.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Telegram\Bot\Traits;
+
+use Telegram\Bot\Api;
+
+/**
+ * Class Telegram
+ */
+trait Telegram
+{
+    /** @var Api Holds the Super Class Instance. */
+    protected $telegram = null;
+
+    /**
+     * Returns Super Class Instance.
+     *
+     * @return Api
+     */
+    public function getTelegram()
+    {
+        return $this->telegram;
+    }
+
+    /**
+     * Set Telegram Api Instance.
+     *
+     * @param Api $telegram
+     *
+     * @return $this
+     */
+    public function setTelegram(Api $telegram)
+    {
+        $this->telegram = $telegram;
+
+        return $this;
+    }
+
+}
