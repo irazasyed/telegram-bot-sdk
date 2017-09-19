fc75b8f3e88b63977febcc325d3e2a1d84a11928
imanghafoori <imanghafoori1@gmail.com>
2017-09-19 23:45:11 +0430

    snake case

--- src/Laravel/config/telegram.php
+++ src/Laravel/config/telegram.php
@@ -34,8 +34,8 @@ return [
         'mybot' => [
             'username' => 'TelegramBot',
             'token'    => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
-            'certificatePath'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-            'webhookUrl'    => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
+            'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
+            'webhook_url'    => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands' => [
                 //Acme\Project\Commands\MyTelegramBot\BotCommand::class
             ],
