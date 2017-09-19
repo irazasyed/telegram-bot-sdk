14bd74c340f6125023337e2a50adbca2fb214d9f
Syed Irfaq R. ⚡️ <irazasyed@users.noreply.github.com>
2017-09-20 04:48:02 +0530

    Refactor & Update Info Method

--- src/Laravel/Artisan/WebhookCommand.php
+++ src/Laravel/Artisan/WebhookCommand.php
@@ -2,22 +2,23 @@
 
 namespace Telegram\Bot\Laravel\Artisan;
 
-use Illuminate\Console\GeneratorCommand as LaravelGeneratorCommand;
-use Symfony\Component\Console\Input\InputOption;
 use Telegram\Bot\BotsManager;
+use Illuminate\Console\Command;
+use Telegram\Bot\Objects\WebhookInfo;
+use Symfony\Component\Console\Helper\TableCell;
 
-class WebhookCommand extends LaravelGeneratorCommand
+class WebhookCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'telegram:webhook {botName : The bot name defined in the config file}
-                {--setup : To declare your webhook on telegram servers. So they can call you.} 
-                {--remove : To remove your already declared webhook on telegram servers.} 
-                {--info : To get the information about your current webhook on telegram servers.}';
-
+    protected $signature = 'telegram:webhook {bot? : The bot name defined in the config file}
+                {--all : To perform actions on all your bots.} 
+                {--setup : To declare your webhook on Telegram servers. So they can call you.} 
+                {--remove : To remove your already declared webhook on Telegram servers.} 
+                {--info : To get the information about your current webhook on Telegram servers.}';
     /**
      * The console command description.
      *
@@ -25,31 +26,35 @@ class WebhookCommand extends LaravelGeneratorCommand
      */
     protected $description = 'Ease the Process of setting up and removing webhooks.';
 
-    private $telegram;
+    /** @var Api */
+    protected $telegram;
 
-    private $config;
+    /** @var BotsManager */
+    protected $botsManager;
+
+    /** @var array Bot Config */
+    protected $config = [];
 
     /**
-     * Create a new command instance.
+     * WebhookCommand constructor.
      *
+     * @param BotsManager $botsManager
      */
-    public function __construct()
+    public function __construct(BotsManager $botsManager)
     {
         parent::__construct();
+
+        $this->botsManager = $botsManager;
     }
 
     /**
      * Execute the console command.
-     *
-     * @param \Telegram\Bot\BotsManager $botManager
-     * @return void
      */
-    public function handle(BotsManager $botManager)
+    public function handle()
     {
-        $botName = $this->argument('botName');
-
-        $this->telegram = $botManager->bot($botName);
-        $this->config = $botManager->getBotConfig($botName);
+        $bot = $this->hasArgument('bot') ? $this->argument('bot') : null;
+        $this->telegram = $this->botsManager->bot($bot);
+        $this->config = $this->botsManager->getBotConfig($bot);
 
         if ($this->option('setup')) {
             $this->setupWebhook();
@@ -64,28 +69,101 @@ class WebhookCommand extends LaravelGeneratorCommand
         }
     }
 
-    private function setupWebhook()
+    /**
+     * Setup Webhook
+     */
+    protected function setupWebhook()
     {
         $params = ['url' => array_get($this->config, 'webhook_url')];
-
         $certificatePath = array_get($this->config, 'certificate_path', false);
+
         if ($certificatePath) {
             $params['certificate'] = $certificatePath;
         }
 
         $response = $this->telegram->setWebhook($params);
+        if ($response) {
+            $this->info('Success: Your webhook has been set!');
 
-        $this->info($response->description);
+            return;
+        }
+
+        $this->error('Your webhook could not be set!');
     }
 
-    private function removeWebHook()
+    /**
+     * Remove Webhook
+     */
+    protected function removeWebHook()
     {
-        $response = $this->telegram->removeWebhook();
+        if ($this->confirm("Are you sure you want to remove the webhook for {$this->config['bot']}?")) {
 
-        $this->info($response->description);
+            $this->info('Removing webhook...');
+
+            if ($this->telegram->removeWebhook()) {
+                $this->info('Webhook removed successfully!');
+
+                return;
+            }
+
+            $this->error('Webhook removal failed');
+        }
     }
 
-    private function getInfo()
+    /**
+     * Get Webhook Info
+     */
+    protected function getInfo()
     {
+        $this->alert('Webhook Info');
+
+        if ($this->hasArgument('bot') && !$this->option('all')) {
+            $response = $this->telegram->getWebhookInfo();
+            $this->makeWebhookInfoResponse($response, $this->config['username']);
+
+            return;
+        }
+
+        if ($this->option('all')) {
+            $bots = $this->botsManager->getConfig('bots');
+            collect($bots)->each(function ($bot, $key) {
+                $response = $this->botsManager->bot($key)->getWebhookInfo();
+                $this->makeWebhookInfoResponse($response, $bot['username']);
+            });
+        }
+    }
+
+    /**
+     * Make WebhookInfo Response for console.
+     *
+     * @param WebhookInfo $response
+     * @param string      $bot
+     */
+    protected function makeWebhookInfoResponse(WebhookInfo $response, string $bot)
+    {
+        $rows = $response->map(function ($value, $key) {
+            $key = title_case(str_replace('_', ' ', $key));
+            $value = is_bool($value) ? $this->mapBool($value) : $value;
+
+            return compact('key', 'value');
+        })->toArray();
+
+        $this->table([
+            [new TableCell('Bot: ' . $bot, ['colspan' => 2])],
+            ['Key', 'Info'],
+        ], $rows);
+    }
+
+    /**
+     * Map Boolean Value to Yes/No.
+     *
+     * @param $value
+     *
+     * @return string
+     */
+    protected function mapBool($value)
+    {
+        return $value ? 'Yes' : 'No';
     }
 }
+
