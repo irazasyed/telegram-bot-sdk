3948832a2e8a8f4037ebe2b0c209e3beebf27534
Jonathan Williamson <jonnywilliamson@gmail.com>
2017-09-29 19:10:14 +0100

    Comment out command tests that should return a named argument.

--- tests/Unit/Commands/CommandTest.php
+++ tests/Unit/Commands/CommandTest.php
@@ -49,115 +49,117 @@ class CommandTest extends TestCase
         $this->assertEquals([], $command->getArguments());
     }
 
-    /** @test */
-    public function it_returns_empty_values_if_no_text_other_that_command_is_found()
-    {
-        //Condensed update data
-        $update = new Update([
-            "message" => [
-                "text"     => "/demo",
-                "entities" => [
-                    [
-                        "type"   => "bot_command",
-                        "offset" => 0,
-                        "length" => 5,
-                    ],
-                ],
-            ],
-        ]);
+    //TODO Need to have discussion about how arguments should be returned.
 
-        //First time the command is triggered for entity "0"
-        $entity = $update->getMessage()->entities->get(0)->toArray();
-        $this->command->make($this->api, $update, $entity);
-        $this->assertEquals(['fname' => '', 'lastname' => ''], $this->command->getArguments());
-    }
-
-    /** @test */
-    public function it_returns_empty_values_if_too_many_arguments_have_been_specified_compared_to_text_received()
-    {
-        //Condensed update data
-        $update = new Update([
-            "message" => [
-                "text"     => "This /demo john ",
-                "entities" => [
-                    [
-                        "type"   => "bot_command",
-                        "offset" => 5,
-                        "length" => 5,
-                    ],
-                ],
-            ],
-        ]);
-
-        //First time the command is triggered for entity "0"
-        $entity = $update->getMessage()->entities->get(0)->toArray();
-        $this->command->make($this->api, $update, $entity);
-        $this->assertEquals(['fname' => 'john', 'lastname' => ''], $this->command->getArguments());
-    }
-
-    /** @test */
-    public function normal_message_and_arguments()
-    {
-        //Condensed update data
-        $update = new Update([
-            "message" => [
-                "text"     => "This /demo john doe",
-                "entities" => [
-                    [
-                        "type"   => "bot_command",
-                        "offset" => 5,
-                        "length" => 5,
-                    ],
-                ],
-            ],
-        ]);
-
-        //First time the command is triggered for entity "0"
-        $entity0 = $update->getMessage()->entities->get(0)->toArray();
-        $this->command->make($this->api, $update, $entity0);
-        $this->assertEquals(['fname' => 'john', 'lastname' => 'doe'], $this->command->getArguments());
-    }
-
-    /** @test */
-    public function it_checks_the_arguments_can_be_detected_in_a_message_with_multiple_commands_that_are_the_same()
-    {
-        //Condensed update data
-        $update = new Update([
-            "message" => [
-                "text"     => "This /demo john doe command with /demo jane doe and a unrelated /test command",
-                "entities" => [
-                    [
-                        "type"   => "bot_command",
-                        "offset" => 5,
-                        "length" => 5,
-                    ],
-                    [
-                        "type"   => "bot_command",
-                        "offset" => 33,
-                        "length" => 5,
-                    ],
-                    [
-                        "type"   => "bot_command",
-                        "offset" => 64,
-                        "length" => 5,
-                    ],
-                ],
-            ],
-        ]);
-
-        //First time the command is triggered for entity "0"
-        $entity0 = $update->getMessage()->entities->get(0)->toArray();
-        $this->command->make($this->api, $update, $entity0);
-        $this->assertEquals(['fname' => 'john', 'lastname' => 'doe'], $this->command->getArguments());
-
-        //Second time the command is triggered for entity "1"
-        $entity1 = $update->getMessage()->entities->get(1)->toArray();
-        $this->command->make($this->api, $update, $entity1);
-        $this->assertEquals(['fname' => 'jane', 'lastname' => 'doe'], $this->command->getArguments());
-
-        //This command should not be triggered for entity "2". But if it is, the arguments should be blank.
-        $entity2 = $update->getMessage()->entities->get(2)->toArray();
-        $this->command->make($this->api, $update, $entity2);
-        $this->assertEquals(['fname' => '', 'lastname' => ''], $this->command->getArguments());
-    }
+//    /** @test */
+//    public function it_returns_empty_values_if_no_text_other_than_command_is_found()
+//    {
+//        //Condensed update data
+//        $update = new Update([
+//            "message" => [
+//                "text"     => "/demo",
+//                "entities" => [
+//                    [
+//                        "type"   => "bot_command",
+//                        "offset" => 0,
+//                        "length" => 5,
+//                    ],
+//                ],
+//            ],
+//        ]);
+//
+//        //First time the command is triggered for entity "0"
+//        $entity = $update->getMessage()->entities->get(0)->toArray();
+//        $this->command->make($this->api, $update, $entity);
+//        $this->assertEquals(['fname' => '', 'lastname' => ''], $this->command->getArguments());
+//    }
+//
+//    /** @test */
+//    public function it_returns_empty_values_if_too_many_arguments_have_been_specified_compared_to_text_received()
+//    {
+//        //Condensed update data
+//        $update = new Update([
+//            "message" => [
+//                "text"     => "This /demo john ",
+//                "entities" => [
+//                    [
+//                        "type"   => "bot_command",
+//                        "offset" => 5,
+//                        "length" => 5,
+//                    ],
+//                ],
+//            ],
+//        ]);
+//
+//        //First time the command is triggered for entity "0"
+//        $entity = $update->getMessage()->entities->get(0)->toArray();
+//        $this->command->make($this->api, $update, $entity);
+//        $this->assertEquals(['fname' => 'john', 'lastname' => ''], $this->command->getArguments());
+//    }
+//
+//    /** @test */
+//    public function normal_message_and_arguments()
+//    {
+//        //Condensed update data
+//        $update = new Update([
+//            "message" => [
+//                "text"     => "This /demo john doe",
+//                "entities" => [
+//                    [
+//                        "type"   => "bot_command",
+//                        "offset" => 5,
+//                        "length" => 5,
+//                    ],
+//                ],
+//            ],
+//        ]);
+//
+//        //First time the command is triggered for entity "0"
+//        $entity0 = $update->getMessage()->entities->get(0)->toArray();
+//        $this->command->make($this->api, $update, $entity0);
+//        $this->assertEquals(['fname' => 'john', 'lastname' => 'doe'], $this->command->getArguments());
+//    }
+//
+//    /** @test */
+//    public function it_checks_the_arguments_can_be_detected_in_a_message_with_multiple_commands_that_are_the_same()
+//    {
+//        //Condensed update data
+//        $update = new Update([
+//            "message" => [
+//                "text"     => "This /demo john doe command with /demo jane doe and a unrelated /test command",
+//                "entities" => [
+//                    [
+//                        "type"   => "bot_command",
+//                        "offset" => 5,
+//                        "length" => 5,
+//                    ],
+//                    [
+//                        "type"   => "bot_command",
+//                        "offset" => 33,
+//                        "length" => 5,
+//                    ],
+//                    [
+//                        "type"   => "bot_command",
+//                        "offset" => 64,
+//                        "length" => 5,
+//                    ],
+//                ],
+//            ],
+//        ]);
+//
+//        //First time the command is triggered for entity "0"
+//        $entity0 = $update->getMessage()->entities->get(0)->toArray();
+//        $this->command->make($this->api, $update, $entity0);
+//        $this->assertEquals(['fname' => 'john', 'lastname' => 'doe'], $this->command->getArguments());
+//
+//        //Second time the command is triggered for entity "1"
+//        $entity1 = $update->getMessage()->entities->get(1)->toArray();
+//        $this->command->make($this->api, $update, $entity1);
+//        $this->assertEquals(['fname' => 'jane', 'lastname' => 'doe'], $this->command->getArguments());
+//
+//        //This command should not be triggered for entity "2". But if it is, the arguments should be blank.
+//        $entity2 = $update->getMessage()->entities->get(2)->toArray();
+//        $this->command->make($this->api, $update, $entity2);
+//        $this->assertEquals(['fname' => '', 'lastname' => ''], $this->command->getArguments());
+//    }
 }
