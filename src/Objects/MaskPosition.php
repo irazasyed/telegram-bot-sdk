c7c4fc7364ebd6738a8f4ab36ffee695e043e785
Jonathan Williamson <jonnywilliamson@gmail.com>
2017-10-12 19:01:03 +0100

    Bot API 3.2 July 21 2017 https://core.telegram.org/bots/api#july-21-2017
    All changes as per this release. Feedback welcome

---
+++ src/Objects/MaskPosition.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+/**
+ * Class MaskPosition.
+ *
+ *
+ * @property string $point       The part of the face relative to which the mask should be placed. One of “forehead”, “eyes”, “mouth”, or “chin”.
+ * @property float  $xShift      Shift by X-axis measured in widths of the mask scaled to the face size, from left to right. For example, choosing -1.0 will place mask just to the left of the default mask position.
+ * @property float  $yShift      Shift by Y-axis measured in heights of the mask scaled to the face size, from top to bottom. For example, 1.0 will place the mask just below the default mask position.
+ * @property float  $scale       Mask scaling coefficient. For example, 2.0 means double size.
+ */
+class MaskPosition extends BaseObject
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function relations()
+    {
+        return [];
+    }
+}
