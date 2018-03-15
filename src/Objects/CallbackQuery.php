9b85fc51de7197157850b0251b93246a373ab3de
Stanislav Strebul <s.strebul@gmail.com>
2018-03-15 18:27:00 +0200

    Added callback query

---
+++ src/Objects/CallbackQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+/**
+ * Class CallbackQuery.
+ *
+ * @method User     getFrom()           Sender
+ * @method Message  getMessage()        (Optional). Message with the callback button that originated the query.
+ * @method string   getData()           (Optional). Data associated with the callback button.
+ */
+class CallbackQuery extends BaseObject
+{
+    /**
+     * @inheritdoc
+     */
+    public function relations()
+    {
+        return [
+            'from' => User::class,
+            'message' => Message::class,
+        ];
+    }
+}
