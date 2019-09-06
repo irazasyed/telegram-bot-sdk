f94e9edcb2518a1f3e271ca38ec69da89e6f27ec
Tsunghan,Tsai <adsl.cai@gmail.com>
2019-09-06 23:11:40 +0800

    laravel helper function change
    array_get function change Arr::get
    str_contains change Str::contains
    snake_case change Str::snake

---
+++ BaseObject.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+/**
+ * Class BaseObject.
+ */
+abstract class BaseObject extends Collection
+{
+    /**
+     * Builds collection entity.
+     *
+     * @param array|mixed $data
+     */
+    public function __construct($data)
+    {
+        parent::__construct($this->getRawResult($data));
+
+        $this->mapRelatives();
+    }
+
+    /**
+     * Property relations.
+     *
+     * @return array
+     */
+    abstract public function relations();
+
+    /**
+     * Get an item from the collection by key.
+     *
+     * @param mixed $key
+     * @param mixed $default
+     *
+     * @return mixed|static
+     */
+    public function get($key, $default = null)
+    {
+        if ($this->offsetExists($key)) {
+            return is_array($this->items[$key]) ? new static($this->items[$key]) : $this->items[$key];
+        }
+
+        return value($default);
+    }
+
+    /**
+     * Map property relatives to appropriate objects.
+     *
+     * @return array|void
+     */
+    public function mapRelatives()
+    {
+        $relations = collect($this->relations());
+
+        if ($relations->isEmpty()) {
+            return false;
+        }
+
+        return $this->items = collect($this->all())
+            ->map(function ($value, $key) use ($relations) {
+                if (!$relations->has($key)) {
+                    return $value;
+                }
+
+                $className = $relations->get($key);
+                return new $className($value);
+            })
+            ->all();
+    }
+
+    /**
+     * Returns raw response.
+     *
+     * @return array|mixed
+     */
+    public function getRawResponse()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Returns raw result.
+     *
+     * @param $data
+     *
+     * @return mixed
+     */
+    public function getRawResult($data)
+    {
+        return Arr::get($data, 'result', $data);
+    }
+
+    /**
+     * Get Status of request.
+     *
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return Arr::get($this->items, 'ok', false);
+    }
+
+    /**
+     * Magic method to get properties dynamically.
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        $action = substr($name, 0, 3);
+        if ($action !== 'get') {
+            return false;
+        }
+        $property = Str::snake(substr($name, 3));
+        $response = $this->get($property);
+
+        // Map relative property to an object
+        $relations = $this->relations();
+        if (null != $response && isset($relations[$property])) {
+            return new $relations[$property]($response);
+        }
+
+        return $response;
+    }
+}
---
+++ BotsManager.php
@@ -0,0 +1,292 @@
+<?php
+
+namespace Telegram\Bot;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+
+/**
+ * Class BotsManager
+ *
+ * @TODO Add methods in docblock for autocompletion from Api file.
+ */
+class BotsManager
+{
+    /**
+     * The config instance.
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * The container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The active bot instances.
+     *
+     * @var Api[]
+     */
+    protected $bots = [];
+
+    /**
+     * TelegramManager constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Set the IoC Container.
+     *
+     * @param $container Container instance
+     *
+     * @return $this
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+
+    /**
+     * Get the configuration for a bot.
+     *
+     * @param string|null $name
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return array
+     */
+    public function getBotConfig($name = null)
+    {
+        $name = $name ?: $this->getDefaultBot();
+
+        $bots = $this->getConfig('bots');
+        if (!is_array($config = Arr::get($bots, $name)) && !$config) {
+            throw new InvalidArgumentException("Bot [$name] not configured.");
+        }
+
+        $config['bot'] = $name;
+
+        return $config;
+    }
+
+    /**
+     * Get a bot instance.
+     *
+     * @param string $name
+     *
+     * @return Api
+     */
+    public function bot($name = null)
+    {
+        $name = $name ?: $this->getDefaultBot();
+
+        if (!isset($this->bots[$name])) {
+            $this->bots[$name] = $this->makeBot($name);
+        }
+
+        return $this->bots[$name];
+    }
+
+    /**
+     * Reconnect to the given bot.
+     *
+     * @param string $name
+     *
+     * @return Api
+     */
+    public function reconnect($name = null)
+    {
+        $name = $name ?: $this->getDefaultBot();
+        $this->disconnect($name);
+
+        return $this->bot($name);
+    }
+
+    /**
+     * Disconnect from the given bot.
+     *
+     * @param string $name
+     *
+     * @return void
+     */
+    public function disconnect($name = null)
+    {
+        $name = $name ?: $this->getDefaultBot();
+        unset($this->bots[$name]);
+    }
+
+    /**
+     * Get the specified configuration value for Telegram.
+     *
+     * @param  string $key
+     * @param  mixed  $default
+     *
+     * @return mixed
+     */
+    public function getConfig($key, $default = null)
+    {
+        return Arr::get($this->config, $key, $default);
+    }
+
+    /**
+     * Get the default bot name.
+     *
+     * @return string
+     */
+    public function getDefaultBot()
+    {
+        return $this->getConfig('default');
+    }
+
+    /**
+     * Set the default bot name.
+     *
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setDefaultBot($name)
+    {
+        array_set($this->config, 'default', $name);
+
+        return $this;
+    }
+
+    /**
+     * Return all of the created bots.
+     *
+     * @return Api[]
+     */
+    public function getBots()
+    {
+        return $this->bots;
+    }
+
+    /**
+     * De-duplicate an array.
+     *
+     * @param array $array
+     *
+     * @return array
+     */
+    protected function deduplicateArray(array $array)
+    {
+        return array_values(array_unique($array));
+    }
+
+    /**
+     * Make the bot instance.
+     *
+     * @param string $name
+     *
+     * @return Api
+     */
+    protected function makeBot($name)
+    {
+        $config = $this->getBotConfig($name);
+
+        $token = Arr::get($config, 'token');
+        $commands = Arr::get($config, 'commands', []);
+
+        $telegram = new Api(
+            $token,
+            $this->getConfig('async_requests', false),
+            $this->getConfig('http_client_handler', null)
+        );
+
+        // Check if DI needs to be enabled for Commands
+        if ($this->getConfig('resolve_command_dependencies', false) && isset($this->container)) {
+            $telegram->setContainer($this->container);
+        }
+
+        $commands = $this->parseBotCommands($commands);
+
+        // Register Commands
+        $telegram->addCommands($commands);
+
+        return $telegram;
+    }
+
+    /**
+     * Builds the list of commands for the given commands array.
+     *
+     * @param array $commands
+     *
+     * @return array An array of commands which includes global and bot specific commands.
+     */
+    protected function parseBotCommands(array $commands)
+    {
+        $globalCommands = $this->getConfig('commands', []);
+        $parsedCommands = $this->parseCommands($commands);
+
+        return $this->deduplicateArray(array_merge($globalCommands, $parsedCommands));
+    }
+
+    /**
+     * Parse an array of commands and build a list.
+     *
+     * @param array $commands
+     *
+     * @return array
+     */
+    protected function parseCommands(array $commands)
+    {
+        if (!is_array($commands)) {
+            return $commands;
+        }
+
+        $commandGroups = $this->getConfig('command_groups');
+        $sharedCommands = $this->getConfig('shared_commands');
+
+        $results = [];
+        foreach ($commands as $command) {
+            // If the command is a group, we'll parse through the group of commands
+            // and resolve the full class name.
+            if (isset($commandGroups[$command])) {
+                $results = array_merge(
+                    $results, $this->parseCommands($commandGroups[$command])
+                );
+
+                continue;
+
+            }
+
+            // If this command is actually a shared command, we'll extract the full
+            // class name out of the command list now.
+            if (isset($sharedCommands[$command])) {
+                $command = $sharedCommands[$command];
+            }
+
+            if (!in_array($command, $results)) {
+                $results[] = $command;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Magically pass methods to the default bot.
+     *
+     * @param string $method
+     * @param array  $parameters
+     *
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return call_user_func_array([$this->bot(), $method], $parameters);
+    }
+}
