54034fbad4ad406042881bbcfef40258f28c97a2
Ali Raghebi <aliraghebiii@gmail.com>
2020-03-31 23:17:45 +0430

    complete poll object properties

--- src/Objects/Poll.php
+++ src/Objects/Poll.php
@@ -6,10 +6,15 @@ namespace Telegram\Bot\Objects;
  * Class Poll.
  *
  *
- * @property string       $id           Unique poll identifier
- * @property string       $question     Poll question, 1-255 characters.
- * @property PollOption[] $options      List of poll options
- * @property bool         $isClosed     True, if the poll is closed.
+ * @property string       $id                     Unique poll identifier
+ * @property string       $question               Poll question, 1-255 characters.
+ * @property PollOption[] $options                List of poll options
+ * @property int          $totalVoterCount        Total number of users that voted in the poll
+ * @property bool         $isClosed               True, if the poll is closed.
+ * @property bool         $isAnonymous            True, if the poll is anonymous.
+ * @property string       $type                   Poll type, currently can be “regular” or “quiz”
+ * @property bool         $allowMultipleAnswers   True, if the poll allows multiple answers.
+ * @property int          $correctOptionId        Optional. 0-based identifier of the correct answer option. Available only for polls in the quiz mode, which are closed, or was sent (not forwarded) by the bot or to the private chat with the bot.
  */
 class Poll extends BaseObject
 {
