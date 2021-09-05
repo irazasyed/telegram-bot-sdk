ebf98c4c49415a35ec13701327dc00eff0d5b4ea
foremtehan <53290883+foremtehan@users.noreply.github.com>
2021-09-05 12:28:47 +0430

    Update GuzzleHttpClient.php

--- src/HttpClients/GuzzleHttpClient.php
+++ src/HttpClients/GuzzleHttpClient.php
@@ -88,7 +88,7 @@ class GuzzleHttpClient implements HttpClientInterface
         } catch (RequestException $e) {
             $response = $e->getResponse();
             
-            Cache::store('file')->put('telegram.sdk.catch', $options);
+            Cache::store('file')->put('telegram.sdk.catch', $e->getMessage());
             
             if (! $response instanceof ResponseInterface) {
                 throw new TelegramSDKException($e->getMessage(), $e->getCode());
