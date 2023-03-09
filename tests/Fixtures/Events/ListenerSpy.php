6580d4987aa7e583226ffa5f663526434b44b40b
Irfaq Syed <irazasyed@users.noreply.github.com>
2023-03-10 00:56:10 +0530

    Decouple fixtures

---
+++ tests/Fixtures/Events/ListenerSpy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Telegram\Bot\Tests\Fixtures\Events;
+
+use League\Event\Listener;
+
+class ListenerSpy implements Listener
+{
+    public array $events = [];
+
+    private ?object $calledWith = null;
+
+    private int $timesCalled = 0;
+
+    public function __invoke(object $event): void
+    {
+        $this->timesCalled++;
+        $this->calledWith = $event;
+        $this->events[$event->eventName()][] = $event;
+    }
+
+    public function numberOfTimeCalled(): int
+    {
+        return $this->timesCalled;
+    }
+
+    public function wasCalledWith(object $event): bool
+    {
+        return $event === $this->calledWith;
+    }
+}
